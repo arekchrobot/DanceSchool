8e3a77ab1da21cc6b921aa0c8d74d13eaf76058b
Arkadiusz Chrobot <arkadiusz.chrobot@contractors.roche.com>
2015-11-17 13:03:51 +0100

    Drobne modyfikacje przy menu

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,4 +1,13 @@
 $('a.menu-item').click(function () {
     $('li.active').removeClass('active');
+    $('li.dropdown').removeClass('active');
     $(this).parent().addClass('active');
+});
+$('a.dropdown-item').click(function () {
+    $('li.active').removeClass('active');
+    $('li.dropdown').addClass('active');
+});
+$('a.navbar-brand').click(function() {
+    $('li.active').removeClass('active');
+    $('li.dropdown').removeClass('active');
 });
