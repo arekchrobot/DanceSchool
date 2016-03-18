e461099f2e14beb381cbb3ecfc64bad96066ac33
Arkadiusz Chrobot <chrobot.arkadiusz@gmail.com>
2016-03-18 19:51:38 +0100

    added cookie law

--- danceschool/src/main/resources/static/js/app/config.js
+++ danceschool/src/main/resources/static/js/app/config.js
@@ -7,4 +7,4 @@ var appServices = angular.module('appServices', [])
             'BALLROOM': 'Towarzyski',
             'SPECIAL_OCCASION': 'Okazje specjalne'
         });
-var appControllers = angular.module('appControllers', ['ui.bootstrap', 'ngAnimate', 'uiGmapgoogle-maps', 'appServices', 'LocalStorageModule']);
+var appControllers = angular.module('appControllers', ['ui.bootstrap', 'ngAnimate', 'uiGmapgoogle-maps', 'appServices', 'LocalStorageModule', 'angular-cookie-law']);
